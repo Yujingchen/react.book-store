3b51be18892e212d3d4aadaa35ff42a86c59ca4b
Chen Yujing <vit7david@outlook.com>
2019-05-21 16:21:00 +0300

    initial react application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/layout/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      Book store
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/layout/Header.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { userInfo } from "os";
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="pos-f-t">
+        <div className="collapse" id="navbarToggleExternalContent">
+          <div className="bg-dark p-4">
+            <h5 className="text-white h4">Collapsed content</h5>
+            <span className="text-muted">Toggleable via the navbar brand.</span>
+          </div>
+        </div>
+        <nav className="navbar navbar-dark bg-info ">
+          <ul className="navbar-nav mr-auto navbar-expand-sm">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Cart
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Userinfo
+              </a>
+            </li>
+          </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarToggleExternalContent"
+            aria-controls="navbarToggleExternalContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
